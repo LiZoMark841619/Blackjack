42b0d62900fdb0d15b16d59b042173e824c6addf
LiZoMark841619 <githubs001@gmail.com>
2024-06-09 20:36:08 +0200

    ok

--- files/blackjack.py
+++ files/blackjack.py
@@ -8,9 +8,9 @@ class Blackjack:
         self._dealers = Dealer()
         self._valid = Valid()
         chosen_dealer = self._valid.get_valid_number(f'Choose your dealer from {self._dealers.get_options}! ', 0, list(self._dealers.get_options.keys())[-1])
-        self.__dealer = self._dealers.get_options[chosen_dealer]
         num_players = self._valid.get_valid_number('Enter the number of players from 1 to 4! ', 1, 4)
         players = [Player() for _ in range(num_players)]
+        self.__dealer = self._dealers.get_options[chosen_dealer]
         self.__players = [player.get_player_name for player in players]
 
     def get_dealer(self) -> str:
@@ -51,20 +51,24 @@ class Game:
     def second_card(self) -> None:
         for player in self.game.get_players():
             self.game.deal_card()
-            self._extracted_hit(player)
+            self._extracted_hit_player(player)
         self.game.deal_card()
         self.dealers_card[self.dealer][0] += self.game.get_dealt_card()
         self.dealers_card[self.dealer][-1] += self.game.get_dealt_card_value()
     
-    def _extracted_hit(self, player: str) -> None:
-            self.players_first_two_cards[player][0] += self.game.get_dealt_card()
-            self.players_first_two_cards[player][-1] += self.game.get_dealt_card_value()
-    
+    def _extracted_hit_player(self, player: str) -> None:
+        self.players_first_two_cards[player][0] += self.game.get_dealt_card()
+        self.players_first_two_cards[player][-1] += self.game.get_dealt_card_value()
+            
+    def _extracted_hit_dealer(self) -> None:
+        self.dealers_card[self.dealer][0] += self.game.get_dealt_card()
+        self.dealers_card[self.dealer][-1] += self.game.get_dealt_card_value()
+        
     def players_hit(self) -> None:
         for player in self.players_first_two_cards:
-            while self.valid.get_valid_string(f'Do you want to hit {player}? Enter yes, or no! ', 'yes', 'no') == 'yes':
+            while self.valid.get_valid_string(f'\nDo you want to hit {player}? Enter yes, or no! ', 'yes', 'no') == 'yes':
                 self.game.deal_card()
-                self._extracted_hit(player)
+                self._extracted_hit_player(player)
                 if self.players_first_two_cards[player][-1] > 21:
                     print(f'{player}, you lost! ')
                     break
@@ -75,8 +79,7 @@ class Game:
         while self.dealers_card[self.dealer][-1] < 16:
             self.game.deal_card()
             if self.dealers_card[self.dealer][-1] > 21: return
-            self.dealers_card[self.dealer][0] += self.game.get_dealt_card()
-            self.dealers_card[self.dealer][-1] += self.game.get_dealt_card_value()
+            self._extracted_hit_dealer()
     
     def view_players_hand(self) -> None:
         for card, value in self.players_first_two_cards.items(): print(card,'->',value)
--- files/main.py
+++ files/main.py
@@ -1,18 +1,17 @@
 from blackjack import Game
 
-def _extracted_from_main_5(game, arg: str = '') -> None:
+def _extracted_from_main_5(game) -> None:
     game.view_players_hand()
-    if arg: print(arg)
     game.view_dealer_hand()
 
 def main():
     game = Game()
-    print('\nFirst card of the players!\n')
+    print('\nFirst card of the players and the dealer!\n')
     game.first_card()
-    _extracted_from_main_5(game, '\nFirst card of the dealer!\n')
-    print('\nFirst and second card of the players!\n')
+    _extracted_from_main_5(game)
+    print('\nFirst and second card of the players and the dealer!\n')
     game.second_card()
-    _extracted_from_main_5(game, '\nFirst and second card of the dealer!\n')
+    _extracted_from_main_5(game)
     game.players_hit()
     game.dealer_hit()
     _extracted_from_main_5(game)
