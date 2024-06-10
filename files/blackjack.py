9cf4b29d3cd550bd06452293ad57d5a777924ef0
LiZoMark841619 <githubs001@gmail.com>
2024-06-10 19:11:07 +0200

    ok

--- files/blackjack.py
+++ files/blackjack.py
@@ -8,11 +8,9 @@ class Blackjack(Valid):
         self._dealers = Dealer()
         dealer = self.get_valid_number(f'Choose your dealer from {self._dealers.get_options}! ', 0, list(self._dealers.get_options.keys())[-1])
         num_players = self.get_valid_number('Enter the number of players from 1 to 4! ', 1, 4)
-        players = [Player() for _ in range(num_players)]
-        bets = [Bet() for _ in range(num_players)]
         self.__dealer = self._dealers.get_options[dealer]
-        self.__players = [player.get_player_name for player in players]
-        self.__bets = [bet.make_your_bet() for bet in bets]
+        self.__players = [player.get_player_name for player in [Player() for _ in range(num_players)]]
+        self.__bets = [bet.make_your_bet() for bet in [Bet() for _ in range(num_players)]]
         self.__players_bets = dict(zip(self.__players, self.__bets))
 
     def get_dealer(self) -> str:
@@ -27,7 +25,7 @@ class Blackjack(Valid):
     def welcome(self) -> None:
         print(f'Welcome to game {self.__class__.__name__} {self.get_players()}! ')
 
-    def deal_card(self) -> None:
+    def deal_card(self) -> tuple:
         card = random.choice(self._cards._deck)
         self._cards._deck.remove(card)
         return card
@@ -53,16 +51,16 @@ class Game:
     def second_card(self) -> None:
         for player in self.game.get_players():
             card = self.game.deal_card()
-            self._extracted_hit_player(player, card)
+            self.update_player_card(player, card)
         card = self.game.deal_card()
-        self._extracted_hit_dealer(card)
+        self.update_dealer_card(card)
     
-    def _extracted_hit_player(self, player: str, card) -> None:
+    def update_player_card(self, player: str, card: tuple) -> None:
         self.players_first_two_cards[player][0] += card
         self.players_first_two_cards[player][-1] += self.game.get_dealt_card_value(card)
         if self.players_first_two_cards[player][-1] < 10 and card == 'A': self.players_first_two_cards[player][-1] += 10
             
-    def _extracted_hit_dealer(self, card) -> None:
+    def update_dealer_card(self, card: tuple) -> None:
         self.dealers_card[self.dealer][0] += card
         self.dealers_card[self.dealer][-1] += self.game.get_dealt_card_value(card)
         if self.dealers_card[self.dealer][-1] < 10 and card == 'A': self.dealers_card[self.dealer][-1] += 10
@@ -70,29 +68,35 @@ class Game:
     def player_hit(self, player) -> None:
         while self.game.get_valid_string(f'\nDo you want to hit {player}? Enter yes, or no! ', 'yes', 'no') == 'yes':
             card = self.game.deal_card()
-            self._extracted_hit_player(player, card)
-            if self.players_first_two_cards[player][-1] > 21: print(f'{player}, you lost! '); break
-            print(self.players_first_two_cards); continue
+            self.update_player_card(player, card)
+            if self.players_first_two_cards[player][-1] > 21:
+                print(f'{player}, you lost! ')
+                break
+            print(self.players_first_two_cards)
+            continue
             
     def dealer_hit(self) -> None:
         while self.dealers_card[self.dealer][-1] < 16:
             card = self.game.deal_card()
-            if self.dealers_card[self.dealer][-1] > 21: return
-            self._extracted_hit_dealer(card)
+            if self.dealers_card[self.dealer][-1] > 21:
+                return
+            self.update_dealer_card(card)
     
     def view_player_hand(self) -> None:
-        for player, card_and_value in self.players_first_two_cards.items(): print(player,'->',card_and_value)
+        for player, card_and_value in self.players_first_two_cards.items():
+            print(player,'->',card_and_value)
     
     def view_dealer_hand(self) -> None:
-        for dealer, card_and_value in self.dealers_card.items(): print(dealer,'->',card_and_value)
+        for dealer, card_and_value in self.dealers_card.items():
+            print(dealer,'->',card_and_value)
             
     def splitting(self):
         pass
     def doubling(self):
         pass
     
-    def get_insurance(self, player: str, insurance: int) -> dict:
+    def get_insurance(self, player: str, insurance: int) -> None:
         self.game.get_players_bets()[player] += insurance
 
-    def surrendering(self, player: str) -> dict:
+    def surrendering(self, player: str) -> None:
         self.game.get_players_bets()[player] *= 0.5
