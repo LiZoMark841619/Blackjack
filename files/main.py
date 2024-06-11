fa1d55473b87867891d4b38225b51980f1eeacce
LiZoMark841619 <githubs001@gmail.com>
2024-06-11 09:16:34 +0200

    ok

--- files/main.py
+++ files/main.py
@@ -1,36 +1,39 @@
 from blackjack import Game
 
-blackjack = Game()
-bets = blackjack.game.get_bets()
+class Process:
+    def __init__(self) -> None:
+        self.blackjack = Game()
+        self.bets = self.blackjack.game.get_bets()
 
-def view_hands(game) -> None:
-    game.view_player_hand()
-    game.view_dealer_hand()
+    def view_hands(self) -> None:
+        self.blackjack.view_player_hand()
+        self.blackjack.view_dealer_hand()
 
-def game():
-    print(f'You can see your bets as follows:\n{bets}')
-    print('\nFirst card of the players and the dealer!\n')
-    
-def first_round():
-    blackjack.first_card()
-    for player in blackjack.game.get_players():
-        if blackjack.dealers_card[blackjack.dealer][0][0] == 'A' and blackjack.game.get_valid_string('Would you like to get insurance? Enter yes or no! ', 'yes', 'no') == 'yes':
-            blackjack.get_insurance(player, bets//2)
+    def game(self) -> None:
+        print(f'You can see your bets as follows:\n{self.bets}')
+        print('\nFirst card of the players and the dealer!\n')
+        
+    def first_round(self):
+        self.blackjack.first_card()
+        for player in self.blackjack.game.get_players():
+            if self.blackjack.dealers_card[self.blackjack.dealer][0][0] == 'A' and self.blackjack.game.get_valid_string('Would you like to get insurance? Enter yes or no! ', 'yes', 'no') == 'yes':
+                self.blackjack.get_insurance(player, self.bets//2)
 
-def second_round():
-    print('\nFirst 2 cards of the players and the dealer!\n')
-    blackjack.second_card()
-    view_hands(blackjack)
-    for player in blackjack.game.get_players():
-        if blackjack.game.get_valid_string(f'Would you like to surrender your cards {player}? Enter yes or no! ', 'yes', 'no') == 'yes':
-            blackjack.surrendering(player)
-        else: blackjack.player_hit(player)
-    blackjack.dealer_hit()
+    def second_round(self) -> None:
+        print('\nFirst 2 cards of the players and the dealer!\n')
+        self.blackjack.second_card()
+        self.view_hands()
+        for player in self.blackjack.game.get_players():
+            if self.blackjack.game.get_valid_string(f'Would you like to surrender your cards {player}? Enter yes or no! ', 'yes', 'no') == 'yes':
+                self.blackjack.surrendering(player)
+            else: self.blackjack.player_hit(player)
+        self.blackjack.dealer_hit()
     
 if __name__ == '__main__':
-    game()
-    first_round()
-    view_hands(blackjack)
-    second_round()
-    view_hands(blackjack)
-    print(f'Here are your winnings:\n{blackjack.game.get_bets()}')
+    process = Process()
+    process.game()
+    process.first_round()
+    process.view_hands()
+    process.second_round()
+    process.view_hands()
+    print(f'Here are your winnings:\n{process.blackjack.game.get_bets()}')
